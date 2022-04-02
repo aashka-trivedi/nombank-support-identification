6a1b527c97610cdc4ccab90843a887ff4eab193d
Aashka Trivedi <aashka.trivedi@gmail.com>
2022-04-01 22:25:48 -0400

    Preprocessing: get support sentences

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nombank Support Identification
+
+**Collaborators: Aashka Trivedi, Raksha Hegde, Sarvani Nadiminty**
+
+## Data
+
+- `part-dev`, `part-test`, `part-training` are the data files with sentences containing all partitives.
+- `support-dev`, `support-test`, `support-training` are data files that contain only sentences with a support for the argument/predicate.
---
+++ code/preprocessing.py
@@ -0,0 +1,43 @@
+import argparse
+
+
+def getSupportSentences(filename):
+    '''
+    Gets only those sentences having a support from filename
+    '''
+    training_corpus = open(filename,'r')
+    lines  = training_corpus.readlines()
+    training_corpus.close()
+    sentence = []
+    support_sentences = []
+    contains_support = False
+
+    for line in lines:
+
+        sentence.append(line)
+        if line.strip().split("\t")[-1] == "SUPPORT":
+            contains_support = True
+        
+        if line =="\n":
+            if contains_support:
+                support_sentences.append(sentence)
+                contains_support = False
+            sentence = []
+    return support_sentences
+
+def printSentences(sentences):
+    for sentence in sentences:
+        for line in sentence:
+            print(line, end="")
+    return
+
+
+if __name__ == '__main__':
+
+    #Construct argument parser and parse arguments
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--inputfile", help="Input File")
+    args = vars(parser.parse_args())
+
+    printSentences(getSupportSentences(args["inputfile"]))
